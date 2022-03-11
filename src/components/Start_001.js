db567fcaaf80b252d26fb3d88e99c04a3b842f28
klina8575 <klina8575@gmail.com>
2022-03-11 12:22:29 +0900

    up

---
+++ src/components/Start_001.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Start_001 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Start React!</h1>
+                <p>HTML 적용하기</p>
+            </div>
+        );
+    }
+}
+
+export default Start_001;
