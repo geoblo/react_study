0efc231c9a3baf2fb12c3c93eb982ab4246f5f9a
klina8575 <klina8575@gmail.com>
2022-03-22 11:46:38 +0900

    up

--- src/components/hooks/Hooks_026.js
+++ src/components/hooks/Hooks_026.js
@@ -12,7 +12,7 @@ export default function Hooks026() {
     { name: 'Hanna', age: 28 },
   ]);
 
-  // useState 때문에 전부 재렌더링됨 하지만 이때 persons가 똑같으면 굳이 sum을 계속 실행할 필요가 없다.
+  // useState 할때 컴포넌트가 재렌더링됨 하지만 이때 persons가 똑같으면 굳이 sum을 계속 실행할 필요가 없다.
   const count = sum(persons);
   // const count = useMemo(() => {
   //   return sum(persons);
